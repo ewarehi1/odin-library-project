72a845c9864dd6b5adeb6b143279ebdc40f804a5
ewarehi1 <128170875+ewarehi1@users.noreply.github.com>
2024-05-24 16:46:06 -0600

    Add header and style to page

--- library.js
+++ library.js
@@ -1,6 +1,8 @@
 const library = [
     new Book("The Hobbit", "J. R. R. Tolkien", 304, true),
-    new Book("1984", "George Orwell", 328, false)
+    new Book("1984", "George Orwell", 328, false),
+    new Book("Infinite Jest", "David Foster Wallace", 1079, false),
+    new Book("The Gunslinger", "Stephen King", 251, true)
 ];
 
 function Book(title, author, pages, read) {
@@ -10,20 +12,9 @@ function Book(title, author, pages, read) {
     this.read = read
 };
 
-// const theHobbit = new Book("The Hobbit", "J. R. R. Tolkien", 304, true);
-// const nineteenEightyFour = new Book("1984", "George Orwell", 328, false);
-
-// library.push(theHobbit, nineteenEightyFour);
-
 function addBookToLibrary(book) {
     library.push(book)
 }
-
-const infiniteJest = new Book("Infinite Jest", "David Foster Wallace", 1079, false);
-
-addBookToLibrary(infiniteJest);
-
-addBookToLibrary(new Book("The Gunslinger", "Stephen King", 251, true));
 
 function removeBook(title) {
     for (let book of library) {
@@ -33,4 +24,3 @@ function removeBook(title) {
         }
     }
 }
-
