f42b89d706c3189e99e6526229e448d2512f05c3
ewarehi1 <128170875+ewarehi1@users.noreply.github.com>
2024-05-24 14:33:56 -0600

    Create book constructor and library

--- library.js
+++ library.js
@@ -1,1 +1,14 @@
-console.log("test");
+const library = [];
+
+function Book(title, author, pages, read) {
+    this.name = title,
+    this.author = author,
+    this.pages = pages,
+    this.read = read
+};
+
+const theHobbit = new Book("The Hobbit", "J. R. R. Tolkien", 304, true);
+const nineteenEightyFour = new Book("1984", "George Orwell", 328, false);
+const infiniteJest = new Book("Infinite Jest", "David Foster Wallace", 1079, false);
+
+library.push(theHobbit, nineteenEightyFour, infiniteJest);
